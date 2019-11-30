88a4c93dacdadf29f9df048c7af2bbed3e28a66a
Konstantin Stefanov <kosio7@gmail.com>
2019-11-30 13:48:04 +0200

    Add ts interface and get data from response

--- index.ts
+++ index.ts
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const todoJsonUrl = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(todoJsonUrl).then(response => {
-  console.log(response.data);
+  const data = response.data as Todo;
+  const id = data.id;
+  const title = data.title;
+  const completed = data.completed;
+
+  console.log(`
+    The todo with and id of: ${id}
+    Has a title: ${title}
+    Is it completed: ${completed}
+  `);
 });
